a06d149fc2baff9760df68f71be638058fd650ed
eduardo.vital_brasil <eduardo.vital_brasil@mines-paristech.fr>
2021-10-10 11:57:58 +0200

    Some corrections to the code

--- src/Bird.hpp
+++ src/Bird.hpp
@@ -7,22 +7,22 @@ using vec2 = std::array<float, 2>;
 
 class Bird {
 private:
-	vec2 position;
-	vec2 velocity;
-	vec2 nextVelocity; // A copier dans velocity
-	double bodySize = 1;
-	double viewAngle;
-	double COHESION_RANGE;
-	double NEIGHBOR_RANGE;
-	double SEPARATION_RANGE;
-	double ALIGNMENT_RELAXATION;
-	double COHESION_RELAXATION;
+	vec2 m_position;
+	vec2 m_velocity;
+	vec2 m_nextVelocity; // A copier dans velocity
+	double m_bodySize = 1;
+	double m_viewAngle;
+	double m_COHESION_RANGE;
+	double m_NEIGHBOR_RANGE;
+	double m_SEPARATION_RANGE;
+	double m_ALIGNMENT_RELAXATION;
+	double m_COHESION_RELAXATION;
 
-	std::vector<Bird> neighbors;
+	std::vector<Bird> m_neighbors;
 
 public:
 	Bird(); // Constructeur avec argument par défaut
-	Bird(vec2 position, vec2 velocity);
+	Bird(vec2 m_position, vec2 m_velocity);
 	~Bird(); // Destructeur
 
 public:
@@ -30,26 +30,28 @@ public:
 	vec2 getVelocity();
 
 	void computeNeighbors();
-	vec2 vec_cohesion = cohesion(std::vector<Bird> neighbors);
-	vec2 vec_alignment = alignment(std::vector<Bird> neighbors);
-	vec2 vec_separation = separation(std::vector<Bird> neighbors);
-	void updateVelocity(vec2 vec_cohesion, vec2 vec_alignment, vec2 vec_separation);
+	vec2 m_vec_cohesion = cohesion(neighbors);
+	vec2 m_vec_alignment = alignment(neighbors);
+	vec2 m_vec_separation = separation(neighbors);
+	void updateVelocity(m_vec_cohesion, m_vec_alignment, m_vec_separation);
 	void updatePosition()
 };
 
-void Bird::updateVelocity(vec2 vec_cohesion, vec2 vec_alignment, vec2 vec_separation) {
-	vec2 vec_displacement = vec_cohesion + vec_alignment + vec_separation;
-	velocity += vec_displacement;
+void Bird::updateVelocity(vec2 m_vec_cohesion, vec2 m_vec_alignment, vec2 m_vec_separation) {
+	vec2 vec_displacement = m_vec_cohesion + m_vec_alignment + m_vec_separation;
+	m_velocity += vec_displacement;
 };
 
 void Bird::updatePosition() {
-	position += velocity;
+	m_position += m_velocity;
 };
 
 
 class Flock {
 
 public:
+	int m_popsize;
+	const int m_init_pop_size;
 	void create_population();
 	void calculate_positions();
 	void add_agent();
@@ -63,18 +65,20 @@ public:
 	~Flock(); // Destructeur
 };
 
-Flock::Flock(int popsize) {
-	std::array<float, popsize> birds_vec;
-	std::array<float, popsize> next_pos;
+Flock::Flock(int m_popsize) {
+	const int m_init_popsize = m_popsize; 
+	std::array<Bird, m_init_popsize> m_birds_vec; // Why error if ini_popsize is a const
+	std::array<float, m_init_popsize> m_next_pos;
+	
 	create_population();
 };
 
 Flock::create_population() {
-	for (int i = 0; i < popsize; ++i)
+	for (int i = 0; i < m_popsize; ++i)
 	{
 		vec2 position = random(0, 100);
 		vec2 velocity = random(0, 5);
 		Bird bird = Bird(position, velocity);
-		birds_vec.push_back(bird);
+		m_birds_vec[i] = bird;  // Does it modify the class attribute?
 	}
 };
