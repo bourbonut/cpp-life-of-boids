6901e08cfd1888c68b8830908b3366c34f6c8bd0
eduardo.vital_brasil <eduardo.vital_brasil@mines-paristech.fr>
2021-10-12 10:39:27 +0200

    référence de bird

--- src/Flock.cpp
+++ src/Flock.cpp
@@ -46,7 +46,7 @@ void Flock::updatePositions(){
 void Flock::addAgent() {
 	Vec2 position = Vec2(5, 10);  //random(0, 100);
 	Vec2 velocity = Vec2(-2, 1);  //random(0, 5);
-	Bird bird = Bird(position, velocity);
+	Bird &bird = Bird(position, velocity);
 	m_birdsVec.push_back(bird);
 	// m_nextPos.push_back(position);
 	m_popSize += 1;
