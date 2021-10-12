437bae116e6bd65ef7a21dfa4ce3300c924275e6
Aymeric <aymeric.millan@mines-paristech.fr>
2021-10-12 20:43:25 +0200

    finalizing integration of benjamin's branch and master (1/2) Lost the "Vec2" file with uppercase and have to copy paster from the other branch

--- src/myMath/Vec2.hpp
+++ src/myMath/Vec2.hpp
@@ -1,28 +1,45 @@
 #pragma once
+#include <iostream>
 
-class Vec2 {
-private:
-    double m_x;
-    double m_y;
+typedef struct Vec2 {
+    float x;
+    float y;
 
 public:
     Vec2();
     Vec2(float, float);
-    Vec2(const Vec2& other);
+    Vec2(float);
+    ~Vec2() {};
 
 public:
-    float x() const; // return the x value
-    float y() const; // return the y value
-    Vec2 operator+(const Vec2 &other); // add vectors
-    Vec2 operator-(const Vec2 &other) const; // substract vectors
-    Vec2 operator*(const Vec2 &other); // multiply vectors term by term
-    Vec2 operator*(const float &other); // scale the vector
-    Vec2 operator/(const float &other); // scale the vector
-    Vec2& operator=(const Vec2 &other); // assign vector
-    float dot(Vec2 other); // return the scalar product
-    float norm() const; // return the norm of the vector
+    Vec2 operator+(const Vec2& other); // add vectors
+    Vec2& operator+=(const Vec2& other); // add vectors
+
+    Vec2 operator-(const Vec2& other); // substract vectors
+    Vec2& operator-=(const Vec2& other); // substract vectors
+
+    Vec2 operator*(const Vec2& other); // multiply vectors term by term
+    Vec2 operator*(float other); // scale the vector
+
+    Vec2 operator/(const Vec2& other); // divide vectors term by term
+    Vec2 operator/(float other); // scale the vector
+
+    Vec2& operator*=(const Vec2& other); // multiply vectors term by term
+    Vec2& operator*=(float other); // scale the vector
+
+    Vec2& operator/=(const Vec2& other); // divide vectors term by term
+    Vec2& operator/=(float other); // scale the vector
+
+    Vec2& operator=(const Vec2& other); // assign vector
+    bool operator==(const Vec2& other); // compare current vector with vector
+
+    float dot(const Vec2& other); // return the scalar product
+    float norm(); // return the norm of the vector
     Vec2 normalize(); // return the current vector normalized
     float angle(); // return the angle between the current vector and `Vec2(1, 0)`
-    float anglebt(Vec2 other); // return the angle between the current vector and an other vector
-    void print(); // display the vector in shell
+    float angle(Vec2& other); // return the angle between the current vector and an other vector
+    Vec2 rotate(float angle); // rotate the vector anti-clockwise
+    std::string string(); // return a string useful when you need to display the vector
 };
+
+std::ostream& operator<<(std::ostream& os, Vec2& obj);
