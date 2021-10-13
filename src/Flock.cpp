adec27af7c11eeeef65db12baf20e1b0310d524e
eduardo.vital_brasil <eduardo.vital_brasil@mines-paristech.fr>
2021-10-13 19:03:16 +0200

    Last modifications

--- src/Flock.cpp
+++ src/Flock.cpp
@@ -39,23 +39,10 @@ void Flock::createPopulation() {
 		m_birdsVec.emplace_back(Bird(position, velocity));
 	}
 };
-//here when this will be called by line 21, m_popsize will be 0 so it won't iterate, and reserve here is in the iteration !
-//void Flock::createPopulation() {
-//	for (int i = 0; i < m_popSize; ++i)
-//	{
-//		Vec2 position = Vec2(5, 10);  //random(0, 100);
-//		Vec2 velocity = Vec2(-2, 1);  //random(0, 5);
-//		m_birdsVec[i] = Bird(position, velocity);
-//		m_birdsVec.reserve(m_popSize + 100); // Should we do that?
-//		// m_nextPos[i] = position;
-//	}
-//};
 
 void Flock::calculatePositions() {
 	for (Bird bird : m_birdsVec)
-	{   // To add user interaction, we can include here an if statement according to the bird 
-		// position and use a function similar to updateVelocity. Or, we can add the user 
-		// interaction (like a new bird) directly to computeNeighbors function.
+	{ 
 		const std::vector<Bird> neighbors = this->computeNeighbors(bird, 0,0); //TODO : CHANGE THIS CALL
 		bird.updateVelocity(neighbors);
 		bird.computePosition();
@@ -72,8 +59,8 @@ void Flock::updatePositions() {
 
 //why double && ? and m_popSize is juste m_birdsVec.size(), so no need ?
 void Flock::addAgent() {
-	Vec2&& position = Vec2(5, 10);  //random(0, 100);
-	Vec2&& velocity = Vec2(-2, 1);  //random(0, 5);
+	Vec2 position = Vec2(5, 10);  //random(0, 100);
+	Vec2 velocity = Vec2(-2, 1);  //random(0, 5);
 	m_birdsVec.emplace_back(position, velocity); // emplace_back more efficient than push_back
 };
 
