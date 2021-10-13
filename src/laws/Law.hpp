5bcbeea34955801f97baeb8165fc766605e68e77
Aymeric <aymeric.millan@mines-paristech.fr>
2021-10-13 11:21:38 +0200

    Law class reated

--- src/laws/Law.hpp
+++ src/laws/Law.hpp
@@ -2,18 +2,25 @@
 #include "../myMath/Vec2.hpp"
 //need to add file in CMake.txt
 
-class Law {
+interface Law {
 protected :
 	float m_range;
 	float m_angle;
 	float m_relaxation;
 
 public:
-	Vec2 compute(std::vector<Bird> neighbors, Vec2 currentPos);
+	Vec2 compute(Bird currentBird, std::vector<Bird> neighbors);
 };
 
-class CohesionLaw : public Personnage {
-	Vec2 compute(std::vector<Bird> neighbors, Vec2 currentPos) {
+class CohesionLaw : public Law {
+	Vec2 compute(Bird currentBird, std::vector<Bird> neighbors) {
+		
 		return Vec2(); //je veux ovverdie
+	}
+};
+
+class SeparationLaw : public Law {
+	Vec2 compute(Bird currentBird, std::vector<Bird> neighbors) {
+		
 	}
 };
