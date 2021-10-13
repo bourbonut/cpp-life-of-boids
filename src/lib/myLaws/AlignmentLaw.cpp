4f4b208777aa01e6ec20d843cfd5776038b536f2
Abdeljelil Maadi <abdeljelil.maadi@mines-paristech.fr>
2021-10-13 22:31:13 +0200

    Alignement Law V05

---
+++ src/lib/myLaws/AlignmentLaw.cpp
@@ -0,0 +1,24 @@
+#include "../myMath/Vec2.hpp"
+#include "../myMath/utils.hpp"
+#include <vector>
+class Bird;
+#include "../../resources/Bird.hpp"
+
+
+Vec2 AlignmentLaw::compute(Bird& currentBird, const std::vector<Bird>& neighbors) const {
+
+	float ALIGNMENT_RADIUS = 50;
+		Vec2 nextVelocity(0, 0);
+
+
+	for (int i = 0; i < neighbors.size(); ++i) {
+		if ((currentBird.getPosition() - neighbors[i]) < ALIGNMENT_RADIUS) {
+			Vec2 nextVelocity = currentBird.getVelocity() + neighbors[i];
+			
+		}
+
+		Vec2 nextVelocity = nextVelocity / neighbors.size();
+	}
+
+	return Vec2{};
+};
