654a195d341287c6616ba2d165f1a73a33168cfa
Benjamin Bourbon <benjamin.bourbon@mines-paristech.fr>
2021-10-14 09:44:39 +0200

    Merge branch 4/11

--- tests/test_bird.cpp
+++ tests/test_bird.cpp
@@ -79,4 +79,30 @@ namespace {
 	TEST(TestFlock, NeighborhoodComputing) {
 
 	}
+
+	TEST(Bird, Methods) {
+		Vec2 position = Vec2(1., 1.);
+		Vec2 velocity = Vec2(0., 1.);
+		Bird bird = Bird(position, velocity);
+		std::cout << "Position X: " << bird.getPosition().x() << std::endl;
+		ASSERT_NEAR(bird.getPosition().x(), 1, 1e-6);
+		ASSERT_NEAR(bird.getPosition().y(), 1, 1e-6);
+		ASSERT_NEAR(bird.getNextPosition().x(), 1, 1e-6);
+		ASSERT_NEAR(bird.getNextPosition().y(), 1, 1e-6);
+		ASSERT_NEAR(bird.getVelocity().x(), 0, 1e-6);
+		ASSERT_NEAR(bird.getVelocity().y(), 1, 1e-6);
+		ASSERT_NEAR(bird.getNextVelocity().x(), 0, 1e-6);
+		ASSERT_NEAR(bird.getNextVelocity().y(), 1, 1e-6);
+
+		bird.computePosition();
+		ASSERT_NEAR(bird.getNextPosition().x(), 1, 1e-6);
+		ASSERT_NEAR(bird.getNextPosition().y(), 2, 1e-6);
+
+		bird.updatePosition();
+		ASSERT_NEAR(bird.getPosition().x(), 1, 1e-6);
+		ASSERT_NEAR(bird.getPosition().y(), 2, 1e-6);
+		ASSERT_NEAR(bird.getVelocity().x(), 0, 1e-6);
+		ASSERT_NEAR(bird.getVelocity().y(), 1, 1e-6);
+		//ASSERT_NEAR(a.norm(), 5., 1e-3) << "Norm";
+	}
 }
