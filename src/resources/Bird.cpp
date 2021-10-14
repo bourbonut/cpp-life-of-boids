fdfbbcbbd4aaa6413779f43d13924fb777af4ff1
Aymeric <aymeric.millan@mines-paristech.fr>
2021-10-14 20:46:10 +0200

    fixing bird, switching branch

--- src/resources/Bird.cpp
+++ src/resources/Bird.cpp
@@ -16,10 +16,10 @@ Bird::Bird(const Vec2 &position, const Vec2 &velocity) : m_position{ position },
 										   m_nextPosition{ position }, m_nextVelocity{velocity} {};
 
 Bird::Bird() {
-	m_position = Vec2(0, 0);
-	m_velocity = Vec2(1, 1);
-	m_nextPosition = Vec2(0, 0);
-	m_nextVelocity = Vec2(1, 1);
+	m_position = randomVec2Generation(0, 10);
+	m_velocity = randomVec2Generation(0, 10);
+	m_nextPosition = randomVec2Generation(0, 10);
+	m_nextVelocity = randomVec2Generation(0, 10);
 };
 
 Vec2 Bird::getPosition() const {
@@ -39,10 +39,10 @@ Vec2 Bird::getNextVelocity() const {
 };
 
 void Bird::updateVelocity(const std::vector<Bird> &neighbors) {
-	Vec2 vecCohesion = this->cohesion(neighbors);
-	Vec2 vecAlignment = this->alignment(neighbors);
-	Vec2 vecSeparation = this->separation(neighbors);
-	Vec2 vec_displacement = vecCohesion + vecAlignment + vecSeparation;
+	Vec2 vecCohesion = m_cohesionLaw.compute(*this, neighbors);
+	//Vec2 vecAlignment = this->alignment(neighbors);
+	Vec2 vecSeparation = m_separationLaw.compute(*this, neighbors);
+	Vec2 vec_displacement = vecCohesion + /*vecAlignment +*/ vecSeparation;
 	m_nextVelocity = m_velocity + vec_displacement;
 };
 
@@ -54,64 +54,6 @@ void Bird::updatePosition() {
 	m_position = m_nextPosition;
 	m_velocity = m_nextVelocity;
 };
-
-Vec2 Bird::cohesion(const std::vector<Bird> &neighbors) {
-
-	//We get the barycenter of all the VALID neighbors
-	Vec2 barycenter = computeAgentsBarycenter(neighbors);
-
-	//We calculate the coordinates of the velocity vector
-	//We want to use barycenter.x, or even barycenter - this.position
-	Vec2 newVelocity = { barycenter.x - m_position.x, barycenter.y - m_position.y };
-
-	return newVelocity;
-}
-
-Vec2 Bird::separation(const std::vector<Bird>& neighbors)
-{
-	Vec2 result;
-	float distBetwA = 0, weight = 0;
-
-	//for each neighbor bird, add to a final vector the weigthed input of the neighbor bird
-	for(Bird b : neighbors)
-	{
-		distBetwA = distance(b.getPosition(), m_position);
-		//Get the weight from the inverse of the square, depending on the distance between the two agents
-		weight = 1 / ((distBetwA / m_SEPARATION_RANGE) * (distBetwA / m_SEPARATION_RANGE));
-		//Calculates a vector between agent b and this, and mutiplies it by the current weight of agent b
-		result = result + (m_position - b.getPosition()) * weight;
-	}
-
-	//divide the actual final vector by the number of neighbor birds to get a final vector pointing to the barycenter needed
-	result = result / neighbors.size();
-
-	return result;
-}
-
-Vec2 Bird::computeAgentsBarycenter(const std::vector<Bird> &neighbors) {
-
-	//We create a new array with a size of the number of neighbors
-	std::vector<Vec2> points(neighbors.size());
-	points = getCoordinatesArray(neighbors);
-
-	return barycenter(points);
-}
-
-std::vector<Vec2> Bird::getCoordinatesArray(const std::vector<Bird> &neighbors) {
-	//We create a new array with a size of the number of neighbors
-	std::vector<Vec2> points(neighbors.size());
-
-	for (int i = 0; i < neighbors.size(); ++i) {
-		//filling the array with the coordinates of the position of each agent of the neighborhood
-		float x = (neighbors[i]).getPosition().x;
-		float y = (neighbors[i]).getPosition().y;
-		points[i] = Vec2(x, y);
-		//points[i] = { (((Bird)neighbors[i]).getPosition())[0], (((Bird)neighbors[i]).getPosition())[1] };
-	}
-
-	return points;
-}
-
 
 void Bird::print() const {
 	std::cout << ">>>Printing bird : P(" << m_position.x << ", " << m_position.y << " ) / V(" << m_velocity.x << ", " << m_velocity.y << " )";
--- src/resources/Bird.hpp
+++ src/resources/Bird.hpp
@@ -1,8 +1,9 @@
 #pragma once
-
 #include "Agent.hpp"
 #include "../lib/myMath/Vec2.hpp"
 #include <vector>
+#include "../lib/myLaws/SeparationLaw.hpp"
+#include "../lib/myLaws/CohesionLaw.hpp"
 
 class Bird {
 private:
@@ -12,12 +13,9 @@ private:
 	Vec2 m_nextVelocity;
 	Vec2 m_nextPosition; // A copier dans velocity
 	double m_bodySize { 1 };
-	double m_viewAngle;
-	double m_COHESION_RANGE;
-	double m_NEIGHBOR_RANGE;
-	double m_SEPARATION_RANGE;
-	double m_ALIGNMENT_RELAXATION;
-	double m_COHESION_RELAXATION;
+	CohesionLaw m_cohesionLaw{};// = CohesionLaw{};
+	SeparationLaw m_separationLaw{};// = SeparationLaw{};
+	//AlignmentLaw m_AlignmentLaw{}
 
 
 public:
@@ -32,11 +30,5 @@ public:
 	void updateVelocity(const std::vector<Bird>& neighbors);
 	void computePosition();
 	void updatePosition();
-	Vec2 cohesion(const std::vector<Bird>& neighbors);
-	Vec2 alignment(const std::vector<Bird>& neighbors) { return {}; };
-	Vec2 separation(const std::vector<Bird>& neighbors);
-	std::vector<Vec2> getCoordinatesArray(const std::vector<Bird>& neighbors);
-	//Vec2 computeBarycenter(const std::vector<Vec2> &points);
-	Vec2 computeAgentsBarycenter(const std::vector<Bird> &neighbors);
 	void print() const;
 };
