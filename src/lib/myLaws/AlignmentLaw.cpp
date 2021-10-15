48be16ed2dcefb95c800570248839fa89280983e
Abdeljelil <Abdeljelil.maadi@mines-paristech.fr>
2021-10-15 11:03:52 +0200

    Repare Alignment Law and Fit Ignore

--- src/lib/myLaws/AlignmentLaw.cpp
+++ src/lib/myLaws/AlignmentLaw.cpp
@@ -3,20 +3,21 @@
 #include <vector>
 class Bird;
 #include "../../resources/Bird.hpp"
+#include "AlignmentLaw.hpp"
 
 
-Vec2 AlignmentLaw::computeposition(Bird& currentBird, const std::vector<Bird>& neighbors) const {
+Vec2 AlignmentLaw::compute(Bird& currentBird, const std::vector<Bird>& neighbors) const {
 
 	float ALIGNMENT_RADIUS = 50;
 		Vec2 nextVelocity(0, 0);
 
 
 	for (int i = 0; i < neighbors.size(); ++i) {
-		if ((currentBird.getPosition() - neighbors[i]) < ALIGNMENT_RADIUS) {
-			Vec2 nextVelocity = currentBird.getVelocity() + neighbors[i];
-			Vec2 nextVelocity = nextVelocity / neighbors.size();
-		}
+		//if ((currentBird.getPosition() - neighbors[i].getPosition()) < m_range) {
+			Vec2 nextVelocity = currentBird.getVelocity() + neighbors[i].getVelocity();
+			nextVelocity = nextVelocity / neighbors.size();
+		//}
 	}
 
-	return Vec2{};
+	return nextVelocity;
 };
