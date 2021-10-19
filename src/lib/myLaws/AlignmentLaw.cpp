3d6ac9dd3436f4ffd43a9d9a82b60b059cf72d9c
Arthur VIENS <arthur.viens@mines-paristech.fr>
2021-10-19 09:18:10 +0000

    Dev issue25 tests

--- src/lib/myLaws/AlignmentLaw.cpp
+++ src/lib/myLaws/AlignmentLaw.cpp
@@ -7,15 +7,15 @@
 
 Vec2 AlignmentLaw::compute(Agent& currentAgent, const std::vector<Agent*>& neighbors) const {
 
-		Vec2 nextVelocity(0, 0);
-
-
-	for (int i = 0; i < neighbors.size(); ++i) {
-		//if ((currentBird.getPosition() - neighbors[i].getPosition()) < m_range) {
-			Vec2 nextVelocity = currentAgent.getVelocity() + (*neighbors[i]).getVelocity();
-			nextVelocity = nextVelocity / neighbors.size();
-		//}
+	Vec2 nextVelocity(0, 0);
+	if (neighbors.size() > 0) {
+		for (int i = 0; i < neighbors.size(); ++i) {
+			nextVelocity = nextVelocity + (*neighbors[i]).getVelocity();
+			// std::cout << "I'm here with neighbor " << (*neighbors[i])._id << " its velocity is " << (*neighbors[i]).getVelocity() << " and nextVelocity is " << nextVelocity << '\n';
+		}
+		nextVelocity = nextVelocity / neighbors.size();
 	}
 
+	//std::cout << "Returning nextvelocity : " << nextVelocity << '\n';
 	return nextVelocity;
 };
