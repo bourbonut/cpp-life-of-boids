ebb25cf4eb59798cbff1ad8d7db9bab4e74e5a8f
Julian AURIAC <julian.auriac@mines-paristech.fr>
2021-10-27 16:03:28 +0200

    changed for older tests

--- tests/test_law.cpp
+++ tests/test_law.cpp
@@ -9,7 +9,7 @@
 #include "../src/lib/myMath/Vec2.hpp"
 
 namespace {
-	TEST(AlignmentLaw,BasicsTests) {
+    TEST(AlignmentLaw, BasicsTests) {
         int size = 2;
         Flock* flockPtr = nullptr;
         std::vector<Agent*> mainFlock;
@@ -39,10 +39,10 @@ namespace {
         EXPECT_EQ(alignments[0].x, 0.5f);
         EXPECT_EQ(alignments[0].y, -1.f);
         EXPECT_EQ(alignments[1].x, 0.f);
-        EXPECT_EQ(alignments[1].y, 1.f);
-        EXPECT_EQ(alignments[2].x, 0.5f);
-        EXPECT_EQ(alignments[2].y, 0.f);
-	}
+        EXPECT_EQ(alignments[1].y, 0.f);
+        EXPECT_EQ(alignments[2].x, 1.f);
+        EXPECT_EQ(alignments[2].y, -1.f);
+    }
 
     TEST(CohesionLaw, BasicTests) {
         int size = 2;
@@ -74,8 +74,8 @@ namespace {
         EXPECT_NEAR(cohesions[0].y, 30.f, 1);
         EXPECT_NEAR(cohesions[1].x, -30.f, 1);
         EXPECT_NEAR(cohesions[1].y, -30.f, 1);
-        EXPECT_NEAR(cohesions[2].x, 30.f, 1); //0 because out of range from [1] (60 > 50) and angle from [0] (135,001 > 135 or 135 !< 270/2)
-        EXPECT_NEAR(cohesions[2].y, -30.f, 1);
+        EXPECT_NEAR(cohesions[2].x, 0.f, 1); //0 because out of range from [1] (60 > 50) and angle from [0] (135,001 > 135 or 135 !< 270/2)
+        EXPECT_NEAR(cohesions[2].y, 0.f, 1);
     }
 
     TEST(SeparationLaw, BasicTests) {
@@ -107,9 +107,9 @@ namespace {
 
         EXPECT_EQ(separations[0].x, 0.f);
         EXPECT_NEAR(separations[0].y, -2.f, 0.1f);
-        EXPECT_NEAR(separations[1].x, 1.f, 0.1f);
+        EXPECT_NEAR(separations[1].x, 2.f, 0.1f);
         EXPECT_NEAR(separations[1].y, 1.f, 0.1f);
-        EXPECT_NEAR(separations[2].x, -2.f, 0.1f);  //-1.f and not -2.f because a neighbor is not a viable neighbor
-        EXPECT_NEAR(separations[2].y, 1.f, 0.1f);   //0.f and not -1.f because a neighbor is not a viable neighbor
+        EXPECT_NEAR(separations[2].x, -1.f, 0.1f);  //-1.f and not -2.f because a neighbor is not a viable neighbor
+        EXPECT_NEAR(separations[2].y, 0.f, 0.1f);   //0.f and not -1.f because a neighbor is not a viable neighbor
     }
 }
